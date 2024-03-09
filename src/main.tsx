35eface034493277aacbb7f867ced6c0710614f7
Vladislav <vladalt1231@gmail.com.com>
2024-03-09 11:30:16 +0900

    Add register

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./assets/style.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { persistor, store } from "./store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
       <React.StrictMode>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </Provider>
   </BrowserRouter>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import { feedApi } from "../api/api";
 import { tagSlice } from "./tagSlice";
 import { profileApi } from "../api/ProfileApi";
 import { authApi } from "../api/auth";
 import { authSlice } from "./authSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "Conduit",
+  storage,
+  whitelist: [authSlice.name],
+};
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     [feedApi.reducerPath]: feedApi.reducer,
     [tagSlice.reducerPath]: tagSlice.reducer,
     [profileApi.reducerPath]: profileApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [authSlice.name]: authSlice.reducer,
-  },
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
+    getDefaultMiddleware({ serializableCheck: false }).concat(
       feedApi.middleware,
       profileApi.middleware,
       authApi.middleware
     ),
 });
-
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
